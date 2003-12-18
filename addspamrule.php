7b8b59a94a7bac0e770b12795096339750aa6790
avel <avel@a2757757-2e2c-4c29-8094-907933447b92>
2003-12-18 12:25:54 +0000

    Support for getting SPAM RBLs from LDAP

--- addspamrule.php
+++ addspamrule.php
@@ -8,7 +8,7 @@
  * Licensed under the GNU GPL. For full terms see the file COPYING that came
  * with the Squirrelmail distribution.
  *
- * $Id: addspamrule.php,v 1.2 2003/11/07 16:11:20 avel Exp $
+ * $Id: addspamrule.php,v 1.3 2003/12/18 12:25:54 avel Exp $
  */
 
 /**
@@ -44,6 +44,19 @@ global $spamrule;
 
 /* Spam Rule variables */
 
+
+/* If we need to get spamrule RBLs from LDAP, then do so now. */
+
+if(isset($_SESSION['spamrule_rbls'])) {
+	$spamrule_rbls = $_SESSION['spamrule_rbls'];
+} elseif(isset($spamrule_tests_ldap) && $spamrule_tests_ldap == true &&
+   !isset($_SESSION['spamrule_rbls'])) {
+	include_once('spamrulefunctions.php');
+	$spamrule_rbls = avelsieve_askldapforrbls();
+	$_SESSION['spamrule_rbls'] = $spamrule_rbls;
+}
+
+
 if(isset($_POST['tests'])) {
 	$tests = $_POST['tests'];
 } else {
@@ -62,16 +75,27 @@ if(isset($_POST['action']))  {
 	$action = $spamrule_action_default;
 }
 
+if(isset($_POST['stop']))  {
+	$stop = $_POST['stop'];
+}
 
 /* Other stuff */
 sqgetGlobalVar('sieve_capabilities', $sieve_capabilities, SQ_SESSION);
 
 if(isset($_POST['finished'])) {
 	/* get it together & save it */
+	if($action == 'junk' && isset($_POST['junkprune_saveme'])) {
+		/* Save previously unset (or zero) junkprune variable */
+		setPref($data_dir, $username, 'junkprune', $_POST['junkprune_saveme']);
+	}
+
 	$newrule['type'] = 10;
 	$newrule['tests'] = $tests;
 	$newrule['score'] = $score;
 	$newrule['action'] = $action;
+	if(isset($stop)) {
+		$newrule['stop'] = $stop;
+	}
 	$_SESSION['comm']['new'] = true;
 	$_SESSION['returnnewrule'] = $newrule;
 	header('Location: table.php');
@@ -122,6 +146,16 @@ if(!isset($spamrule_advanced)) {
 	print '<li><strong>';
 	print _("Target Score");
 	print '</strong></li>';
+	
+	/* If using sendmail LDAP configuration, get the sum of maximum score */
+	if(isset($spamrule_rbls)) {
+		$spamrule_score_max = 0;
+		foreach($spamrule_rbls as $no=>$info) {
+			if(isset($info['serverweight'])) {
+				$spamrule_score_max += $info['serverweight'];
+			}
+		}
+	}
 
 	print '<p>'. sprintf( _("Messages with SPAM-Score higher than the target value, the maximum value being %s, will be considered SPAM.") , $spamrule_score_max ) . '<br />';
 
@@ -133,15 +167,33 @@ if(!isset($spamrule_advanced)) {
 	print _("SPAM Lists to check against");
 	print '</strong></li><p>';
 	
-	if(isset($spamrule_tests)) {
-	foreach($spamrule_tests as $st=>$txt) {
-		print '<input type="checkbox" name="tests[]" value="'.$st.'" id="spamrule_test_'.$st.'" ';
-		if(in_array($st, $tests)) {
-			print 'checked="" ';
+	/**
+	 * Print RBLs that are available in this system.
+	 * 1) Check for RBLs in LDAP Sendmail configuration
+	 * 2) Use RBLs supplied in config.php
+	 */
+	 
+	if(isset($spamrule_rbls)) {
+		/* from LDAP */
+		foreach($spamrule_rbls as $no=>$info) {
+			print '<input type="checkbox" name="tests[]" value="'.$info['test'].'" id="spamrule_test_'.$no.'" ';
+			if(in_array($info['test'], $tests)) {
+				print 'checked="" ';
+			}
+			print '/> ';
+			print '<label for="spamrule_test_'.$no.'">'.$info['name'].' ('.$info['serverweight'].')</label><br />';
 		}
-		print '/> ';
-		print '<label for="spamrule_test_'.$st.'">'.$txt.'</label><br />';
-	}
+			
+	} elseif(isset($spamrule_tests)) {
+		/* from config.php */
+		foreach($spamrule_tests as $st=>$txt) {
+			print '<input type="checkbox" name="tests[]" value="'.$st.'" id="spamrule_test_'.$st.'" ';
+			if(in_array($st, $tests)) {
+				print 'checked="" ';
+			}
+			print '/> ';
+			print '<label for="spamrule_test_'.$st.'">'.$txt.'</label><br />';
+		}
 	/* TODO: Import spamrule_filters from Filters plugin */
 	/*
 	} elseif(isset($spamrule_filters)) {
@@ -181,6 +233,29 @@ if(!isset($spamrule_advanced)) {
 	print '</ul>';
 
 }
+
+if(isset($junkprune_saveme)) {
+	print '<input type="hidden" name="junkprune_saveme" value="'.$junkfolder_days.'" />';
+}
+	
+	/* STOP */
+	
+	print '<br /><input type="checkbox" name="stop" id="stop" ';
+	if(isset($stop)) {
+		print 'checked="" ';
+	}
+	print '/> ';
+	print '<label for="stop">';
+	if ($useimages) {
+		print '<img src="images/stop.gif" width="35" height="33" border="0" alt="';
+		print _("STOP");
+		print '" align="middle" /> ';
+	} else {
+		print "<strong>"._("STOP").":</strong> ";
+	}
+	print _("If this rule matches, do not check any rules after it.");
+	print '</label>';
+		
 
 print_section_end();
 print_all_sections_end();
