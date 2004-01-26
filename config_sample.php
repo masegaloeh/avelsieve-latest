747426e4e4cb6ccac527ad392cd0291e4930f54f
avel <avel@a2757757-2e2c-4c29-8094-907933447b92>
2004-01-26 14:43:22 +0000

    Updated and reorganized sample configuration file

--- config_sample.php
+++ config_sample.php
@@ -1,25 +1,28 @@
 <?php
-/*
+/**
  * User-friendly interface to SIEVE server-side mail filtering.
  * Plugin for Squirrelmail 1.4+
  *
- * Copyright (c) 2002-2003 Alexandros Vellis <avel@users.sourceforge.net>
+ * Copyright (c) 2002-2004 Alexandros Vellis <avel@users.sourceforge.net>
  *
  * Licensed under the GNU GPL. For full terms see the file COPYING that came
  * with the Squirrelmail distribution.
  *
- * $Id: config_sample.php,v 1.7 2004/01/22 16:18:59 avel Exp $
+ * $Id: config_sample.php,v 1.8 2004/01/26 14:43:22 avel Exp $
  */
 
-/* Configuration parameters for SIEVE mail filters (aka avelsieve) */
+/**
+ * Configuration parameters for SIEVE mail filters (aka avelsieve)
+ */
+
+/* ======================================================================== */
+/* =================== IMAP Server / SIEVE Setup  ========================= */
+/* ======================================================================== */
 
 /* Port where timsieved listens on the Cyrus IMAP server. Default is 2000. */
 
 $sieveport = 2000;
 
-/* Use images for the move up / down, delete rule buttons and STOP? */
-
-$useimages = true;
 
 /* **** NEW OPTION (as of 0.9.4) ****
  * Space separated list of preferred SASL mechanisms for the authentication to
@@ -46,6 +49,34 @@ $imapproxyserv = array(
 	'localhost' => 'imap.example.org'
 );
 
+/* *** Option Reenabled since 0.9.9 ***
+ * Save changes to SIEVE server immediately or be conservative and save only
+ * when "Save Changes" is selected? */
+
+$conservative = true;
+
+/* Ldapuserdata mode: Gets user's email addresses (including mailAlternate &
+ * mailAuthorized) from LDAP Prefs Backend plugin's cache */
+
+$ldapuserdatamode = false;
+
+/* Map of cyrus administrator users, for proxy authentication */
+
+$cyrusadmins_map = array(
+	'cyrusimap' => 'cyrussieve'
+);
+
+
+
+/* ======================================================================== */
+/* =============== Avelsieve Interface / Behavior Setup  ================== */
+/* ======================================================================== */
+
+
+/* Use images for the move up / down, delete rule buttons and STOP? */
+
+$useimages = true;
+
 /* Translate the messages returned by the "Reject" and "Vacation" actions? The
  * default behaviour since 0.9 is not to translate them. Change to true if in
  * an intranet environment or in a same-language environment. */
@@ -59,8 +90,7 @@ $translate_return_msgs = false;
 $imagetheme = 'bluecurve_24x24';
 //$imagetheme = 'bluecurve_16x16';
 
-/* Beta feature: Enable Create New Folder routine in step #3 of adding a new
- * rule? */
+/* Enable Create New Folder routine in step #3 of adding a new rule? */
 
 $createnewfolder = true;
 
@@ -78,7 +108,7 @@ $maxitems = 10;
 $headers = array(
  'From', 'To', 'Cc', 'Bcc', 'Subject', 'Reply-To', 'Sender', 'List-Id',
  'MailingList', 'X-ML-Name', 'X-List', 'X-Mailer', 'X-MailingList',
- 'X-Spam-Flag', 'X-Spam-Status', 'X-Priority', 'Importance',
+ 'X-Mailing-List', 'X-Spam-Flag', 'X-Spam-Status', 'X-Priority', 'Importance',
  'X-MSMail-Priority', 'Precedence');
 
 /* Available :method's for the :notify extension (if applicable) */
@@ -109,52 +139,46 @@ $disable_avelsieve_capabilities = array();
 
 $avelsieveheaderlink = true;
 
-/* New option as of 0.9.7:
- * Ldapuserdata mode: Gets user's email addresses (including mailAlternate &
- * mailAuthorized) from LDAP Prefs Backend plugin's cache */
+/* *** New option as of 0.9.9 ***
+ * Default display mode, one of 'verbose' or 'terse' */
+$avelsieve_default_mode = 'verbose';
 
-
-$ldapuserdatamode = false;
-
-/**
- * New option as of 0.9.9:
- * Default display mode, one of 'verbose' or 'terse'
- * @var string
- */
-$avelsieve_default_mode = 'terse';
 
 
 /* ======================================================================== */
+/* ========================= Custom rules Configuration =================== */
+/* ======================================================================== */
 
-/* Custom rules */
 
-/* Experimental easy anti-spam rule Configuration. Options should be
+/* Beta - easy anti-spam rule Configuration. Options should be
  * self-explanatory. For $spamrule_tests, the key is the spam block list as
  * displayed in the message header inserted by your anti-spam solution, while
  * the value is the user-friendly name displayed to the user in the advanced
  * configuration. $spamrule_action_default can be one of 'junk', 'trash' or
  * 'discard'. You can set it to 'junk' if you have the Junkfolder plugin
- * installed. */
+ * installed.
+ *
+ * If you would like to get the Spam tests from Sendmail's configuration (which
+ * resides in LDAP), try something like this in your config/config_local.php:
+ *
+ * $ldap_server[0]['mtarblspamfilter'] =
+ *       '(|(sendmailmtaclassname=SpamRBLs)(sendmailmtaclassname=SpamForged))';
+ * $ldap_server[0]['mtarblspambase'] = 'ou=services,dc=example,dc=org';
+ *
+ */
 
 $spamrule_enable = false;
 $spamrule_score_max = 100;
 $spamrule_score_default = 80;
 $spamrule_score_header = 'X-Spam-Score';
+$spamrule_tests_ldap = false; /* Try to ask Sendmail's LDAP Configuration */
 $spamrule_tests = array(
 	'Open.Relay.DataBase' => "Open Relay Database",
-	'Spamhaus.Block.List' => "Spamhaus Block List"
+	'Spamhaus.Block.List' => "Spamhaus Block List",
+        'SpamCop' => "SpamCop",
+        'FORGED' => "Forged Header"
 );
 $spamrule_tests_header = 'X-Spam-Tests';
 $spamrule_action_default = 'trash';
-
-
-
-/* ======================================================================== */
-
-/* ------ Custom / Obsolete Settings (leave alone) ------ */
-
-/* Obsolete (leave this here, though) */
-$conservative = true;
-
 
 ?>
