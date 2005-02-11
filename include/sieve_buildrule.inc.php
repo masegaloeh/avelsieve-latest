6a299e99f800eb3fb8eba8da52b8ed5cf8c2d0b9
avel <avel@a2757757-2e2c-4c29-8094-907933447b92>
2005-02-11 14:57:46 +0000

    Descriptive text for Notify action (short version) now shows the destination address.

--- include/sieve_buildrule.inc.php
+++ include/sieve_buildrule.inc.php
@@ -6,7 +6,7 @@
  * Licensed under the GNU GPL. For full terms see the file COPYING that came
  * with the Squirrelmail distribution.
  *
- * @version $Id: sieve_buildrule.inc.php,v 1.3 2004/11/12 13:31:08 avel Exp $
+ * @version $Id: sieve_buildrule.inc.php,v 1.4 2005/02/11 14:57:46 avel Exp $
  * @author Alexandros Vellis <avel@users.sourceforge.net>
  * @copyright 2004 The SquirrelMail Project Team, Alexandros Vellis
  * @package plugins
@@ -620,7 +620,7 @@ function makesinglerule($rule, $type="rule") {
 		$out .= ":message \"".$rule['notify']['message']."\"";
 		$out .= ";\n";
 	
-		$terse .= "<br />NOTIFY";
+		$terse .= "<br />NOTIFY ".$rule['notify']['options'];
 	}
 	
 	
