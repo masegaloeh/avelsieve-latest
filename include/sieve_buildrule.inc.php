b6d558b1a7873e29dd2e8590f83b81b69fcf0373
avel <avel@a2757757-2e2c-4c29-8094-907933447b92>
2005-03-04 16:02:36 +0000

    Forgot debug output...

--- include/sieve_buildrule.inc.php
+++ include/sieve_buildrule.inc.php
@@ -6,7 +6,7 @@
  * Licensed under the GNU GPL. For full terms see the file COPYING that came
  * with the Squirrelmail distribution.
  *
- * @version $Id: sieve_buildrule.inc.php,v 1.7 2005/03/01 17:46:01 avel Exp $
+ * @version $Id: sieve_buildrule.inc.php,v 1.8 2005/03/04 16:02:36 avel Exp $
  * @author Alexandros Vellis <avel@users.sourceforge.net>
  * @copyright 2004 The SquirrelMail Project Team, Alexandros Vellis
  * @package plugins
@@ -757,7 +757,6 @@ function makesieverule ($rulearray) {
 		}
 		$out .= makesinglerule($rulearray[$i],"rule");
 	}
-	print_r($out);
 	return avelsieve_encode_script($out);
 }
 
