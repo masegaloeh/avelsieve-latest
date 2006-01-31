675d4c8d51c859fcbf56d1cafeeca7c648aa62b1
avel <avel@a2757757-2e2c-4c29-8094-907933447b92>
2006-01-31 11:55:17 +0000

    bugreport email as constant

--- include/constants.inc.php
+++ include/constants.inc.php
@@ -6,12 +6,14 @@
  * Licensed under the GNU GPL. For full terms see the file COPYING that came
  * with the Squirrelmail distribution.
  *
- * @version $Id: constants.inc.php,v 1.11 2006/01/16 10:57:59 avel Exp $
+ * @version $Id: constants.inc.php,v 1.12 2006/01/31 11:55:17 avel Exp $
  * @author Alexandros Vellis <avel@users.sourceforge.net>
  * @copyright 2004 The SquirrelMail Project Team, Alexandros Vellis
  * @package plugins
  * @subpackage avelsieve
  */
+
+define('AVELSIEVE_BUGREPORT_EMAIL', 'avelsieve_bug_report@edunet.gr');
 
 $conditions = array(
 	"and" => _("AND (Every item must match)"),
