93e0ed9be76b208227e8526808894bf22d261598
avel <avel@a2757757-2e2c-4c29-8094-907933447b92>
2007-01-17 10:50:21 +0000

    Split custom spam rule to its own file; this allows for easier defining of customized rules, too.

--- include/sieve_buildrule.inc.php
+++ include/sieve_buildrule.inc.php
@@ -6,7 +6,7 @@
  * Licensed under the GNU GPL. For full terms see the file COPYING that came
  * with the Squirrelmail distribution.
  *
- * @version $Id: sieve_buildrule.inc.php,v 1.29 2007/01/17 09:40:24 avel Exp $
+ * @version $Id: sieve_buildrule.inc.php,v 1.30 2007/01/17 10:50:21 avel Exp $
  * @author Alexandros Vellis <avel@users.sourceforge.net>
  * @copyright 2004 The SquirrelMail Project Team, Alexandros Vellis
  * @package plugins
@@ -360,158 +360,17 @@ function makesinglerule($rule, $mode='rule') {
 	/* Step one: make the if clause */
 	/* The actual 'if' will be added by makesieverule() */
 	
-	if($rule['type'] == '10') {
-		/* SpamRule */
-	
-		global $spamrule_score_default, $spamrule_score_header,
-		$spamrule_tests, $spamrule_tests_header, $spamrule_action_default;
-		
-		$spamrule_advanced = false;
-	
-		if(isset($rule['advanced'])) {
-			$spamrule_advanced = true;
-		}
-	
-		if(isset($rule['score'])) {
-			$sc = $rule['score'];
-		} else {
-			$sc = $spamrule_score_default;
-		}
-		
-		if(isset($rule['tests'])) {
-			$te = $rule['tests'];
-		} else {
-			$te = array_keys($spamrule_tests);
-		}
-	
-		if(isset($rule['action'])) {
-			$ac = $rule['action'];
-		} else {
-			$ac = $spamrule_action_default;
-		}
-	
-		/*
-		if allof( anyof(header :contains "X-Spam-Rule" "Open.Relay.Database" ,
-		        	header :contains "X-Spam-Rule" "Spamhaus.Block.List" 
-				),
-		  	header :value "gt" :comparator "i;ascii-numeric" "80" ) {
-			
-			fileinto "INBOX.Junk";
-			discard;
-		}
-			
-		// Whitelist scenario:
-		if allof( anyof(header :contains "X-Spam-Rule" "Open.Relay.Database" ,
-		        	header :contains "X-Spam-Rule" "Spamhaus.Block.List" 
-				),
-		  	header :value "gt" :comparator "i;ascii-numeric" "80" ,
-		  	not anyof(header :contains "From" "Important Person",
-		        	header :contains "From" "Foo Person"
-		  	)
-			) {
-			
-			fileinto "INBOX.Junk";
-			discard;
-		}
-		*/
-		
-		$out .= 'allof( ';
-		$text .= _("All messages considered as <strong>SPAM</strong> (unsolicited commercial messages)");
-		$terse .= _("SPAM");
-		$tech .= 'SPAM';
-		
-		if(sizeof($te) > 1) {
-			$out .= ' anyof( ';
-			for($i=0; $i<sizeof($te); $i++ ) {
-				$out .= 'header :contains "'.$spamrule_tests_header.'" "'.$te[$i].'"';
-				if($i < (sizeof($te) -1 ) ) {
-					$out .= ",";
-				}
-			}
-			$out .= " ),\n";
-		} else {
-			$out .= 'header :contains "'.$spamrule_tests_header.'" "'.$te[0].'", ';
-		}
-	
-		$out .= "\n";
-		$out .= ' header :value "ge" :comparator "i;ascii-numeric" "'.$spamrule_score_header.'" "'.$sc.'" ';
-	
-		if(isset($rule['whitelist']) && sizeof($rule['whitelist']) > 0) {
-			/* Insert here header-match like rules, ORed of course. */
-			$text .= ' (' . _("unless") . ' ';
-			$terse .= '<br/>' . _("Whitelist:") . '<ul style="margin-top: 1px; margin-bottom: 1px;">';
-			$tech .= ' !(WHITELIST:<br/>';
-	
-			$out .= " ,\n";
-			$out .= ' not anyof( ';
-			for($i=0; $i<sizeof($rule['whitelist']); $i++ ) {
-				$out .= build_rule_snippet('header', $rule['whitelist'][$i]['header'], $rule['whitelist'][$i]['matchtype'],
-					$rule['whitelist'][$i]['headermatch'] ,'rule');
-				$text .= build_rule_snippet('header', $rule['whitelist'][$i]['header'], $rule['whitelist'][$i]['matchtype'],
-					$rule['whitelist'][$i]['headermatch'] ,'verbose');
-				$terse .= '<li>'. build_rule_snippet('header', $rule['whitelist'][$i]['header'], $rule['whitelist'][$i]['matchtype'],
-					$rule['whitelist'][$i]['headermatch'] ,'terse') . '</li>';
-				$tech .= build_rule_snippet('header', $rule['whitelist'][$i]['header'], $rule['whitelist'][$i]['matchtype'],
-					$rule['whitelist'][$i]['headermatch'] ,'tech') . '<br/>';
-				if($i<sizeof($rule['whitelist'])-1) {
-					$out .= ', ';
-					$text .= ' ' . _("or") . ' ';
-				}
-			}
-			$text .= '), '; 
-			$tech .= '), '; 
-			$terse .= '</ul>'; 
-			$out .= " )";
-		}
-		$out .= " )\n{\n";
-
-		if($spamrule_advanced == true) {
-			$text .= _("matching the Spam List(s):");
-			$terse .= '<br/>' . _("Spam List(s):") . '<ul style="margin-top: 1px; margin-bottom: 1px;">';
-			$tech .= '<br/>' . _("Spam List(s):") . '<ul style="margin-top: 1px; margin-bottom: 1px;">';
-			for($i=0; $i<sizeof($te); $i++) {
-				$text .= $spamrule_tests[$te[$i]].', ';
-				$terse .= '<li>' . $spamrule_tests[$te[$i]].'</li>';
-				$tech .= '<li>' . $spamrule_tests[$te[$i]].'</li>';
-			}
-			$text .= sprintf( _("and with score greater than %s") , $sc );
-			$terse .= '</ul>' . sprintf( _("Score > %s") , $sc);
-			$tech .= '</ul>' . sprintf( _("Score > %s") , $sc);
-		}
-	
-		$text .= ', ' . _("will be") . ' ';
-		$terse .= '</td><td align="right">';
-		$tech .= '</td><td align="right">';
-	
-		if($ac == 'junk') {
-			$out .= 'fileinto "INBOX.Junk";';
-			$text .= _("stored in the Junk Folder.");
-			$terse .= _("Junk");
-			$tech .= 'JUNK';
-	
-		} elseif($ac == 'trash') {
-			$text .= _("stored in the Trash Folder.");
-	
-			global $data_dir, $username;
-			$trash_folder = getPref($data_dir, $username, 'trash_folder');
-			/* Fallback in case it does not exist. Thanks to Eduardo
-		 	* Mayoral. If not even Trash does not exist, it will end up in
-		 	* INBOX... */
-			if($trash_folder == '' || $trash_folder == 'none') {
-				$trash_folder = "Trash";
-			}
-			$out .= 'fileinto "'.$trash_folder.'";';
-
-			$terse .= _("Trash");
-			$tech .= 'TRASH';
-	
-		} elseif($ac == 'discard') {
-			$out .= 'discard;';
-			$text .= _("discarded.");
-			$terse .= _("Discard");
-			$tech .= _("Discard");
-		}
-	
+	if(is_numeric($rule['type']) && $rule['type'] >= '10' && $rule['type'] < 100 ) {
+        include_once(SM_PATH . 'plugins/avelsieve/include/sieve_buildrule.'.$rule['type'].'.inc.php');
+        $res = call_user_func('avelsieve_buildrule_'.$rule['type'], $rule);
+        // Return value is: array($out, $text, $terse, $tech)
+        if($res != false) {
+            $out .= $res[0];
+            $text .= $res[1];
+            $terse .= $res[2];
+            $tech .= $res[3];
+        }
+        
 	} else {
 		$text .= "<strong>"._("If")."</strong> ";
 	} 
