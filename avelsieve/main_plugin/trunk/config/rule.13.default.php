6e38fe3161f34f22764360b9a8b81a15f4dc8d81
avel <avel@a2757757-2e2c-4c29-8094-907933447b92>
2007-03-15 16:07:46 +0000

    Split configuration files into rule-specific settings files.

---
+++ avelsieve/main_plugin/trunk/config/rule.13.default.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User-friendly interface to SIEVE server-side mail filtering.
+ * Plugin for Squirrelmail 1.4+
+ *
+ * Licensed under the GNU GPL. For full terms see the file COPYING that came
+ * with the Squirrelmail distribution.
+ *
+ * Configuration File for Rule #13: Custom Sieve Code
+ *
+ * @version $Id: rule.13.default.php,v 1.1 2007/03/15 16:07:46 avel Exp $
+ * @author Alexandros Vellis <avel@users.sourceforge.net>
+ * @copyright 2002-2007 Alexandros Vellis
+ * @package plugins
+ * @subpackage avelsieve
+ */
+
+/**
+ * @var array Rule #13 (Custom Sieve Code)
+ */
+$avelsieve_rules_settings[13] = array(
+);
+
+?>
