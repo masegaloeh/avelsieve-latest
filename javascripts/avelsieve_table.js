368ff6c06e3f38e56f5e07b79cad9bcd0debb98e
avel <avel@a2757757-2e2c-4c29-8094-907933447b92>
2007-03-19 12:06:22 +0000

    New javascript function for rules table additional options

--- javascripts/avelsieve_table.js
+++ javascripts/avelsieve_table.js
@@ -1,24 +1,17 @@
-function show_hide_column(tableID, col_no, className) {
-    var table  = document.getElementById(tableID);
-    var rows = table.getElementsByTagName('tr');
-    var cells;
- 
-        console.info("total length.... %d", rows.length);
-    for (var row=0; row<rows.length;row++) {
-        cells = rows[row].getElementsByTagName('td');
-
-        console.info("comparing.... %s vs %s ", cells[col_no].className, className);
-        if(cells[col_no].className != className) {
-            console.info("continuing.... col %d, row %d ", col_no, row);
-            continue;
+function avelsieveHandleOptionsSelect(selectElement, num) {
+    var selectedOption;
+    selectedOption =  selectElement.options[selectElement.selectedIndex].value;
+    if(selectedOption) {
+        if(selectedOption == 'mvposition') {
+            var position; 
+            position = prompt('Which Position?'); 
+            if(position) {
+                document.forms[0].position.value = position;
+            } else {
+                return false;
+            }
         }
- 
-        if(cells[col_no].style.display!="none"){
-            cells[col_no].style.display="none";
-            console.log("setting row %d to none", row);
-        }else{
-            cells[col_no].style.display="block";
-            console.log("setting row %d to block", row);
-        }
+        document.forms[0].submit();
     }
-} 
+    return true;
+}
